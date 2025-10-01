39a9e2d810faeefea1a978a5eec2eda9011fd7e0
Ziyad Cheto <154470959+ZiyadCh@users.noreply.github.com>
2025-10-01 23:07:29 +0100

    Add files via upload

--- SAS.c
+++ SAS.c
@@ -7,8 +7,7 @@ char option;
 int count = 20;
 int mid = 21;
 
-struct Animal
-{
+struct Animal {
   int id;
   char nom[50];
   char espece[50];
@@ -17,8 +16,7 @@ struct Animal
   float poids;
 };
 
-struct Animal ajouter()
-{
+struct Animal ajouter() {
   struct Animal p;
 
   p.id = mid++;
@@ -28,12 +26,11 @@ struct Animal ajouter()
   printf("--------------------------------\n");
 
   printf("-Entrer l'age:\n");
-  scanf("%d", &p.age);
-
-  while (scanf("%d",&p.age) != 1 ) {
+  while (scanf("%d", &p.age) != 1) {
     getchar();
-    printf("re-Entrerr l'age\n");
+    printf("re-Entrerr age!!\n");
   }
+
   printf("-Entrer le nom d'animaux:\n");
   scanf("%s", p.nom);
   p.nom[0] = toupper(p.nom[0]);
@@ -44,55 +41,45 @@ struct Animal ajouter()
   scanf("%s", p.habitat);
   p.habitat[0] = toupper(p.habitat[0]);
   printf("-Entrer le poids d'animal:\n");
-  scanf("%f", &p.poids);
-  while (scanf("%f",&p.poids) != 1 ) {
+  while (scanf("%f", &p.poids) != 1) {
     getchar();
     printf("re-Entrerr le poids\n");
   }
 
- 
   return p;
 };
 
-void ajp(struct Animal p[])
-{
+void ajp(struct Animal p[]) {
   int time;
   printf("--------------------------------\n");
   printf("Nombre d'animaux a ajouter:\n");
   scanf("%d", &time);
-  for (int i = 0; i < time; i++)
-  {
+  for (int i = 0; i < time; i++) {
     printf("--------- Animal %d -----\n", i + 1);
     p[count] = ajouter();
     count++;
   }
 };
 
-void affid(struct Animal p[])
-{
+void affid(struct Animal p[]) {
   struct Animal tmp;
-  for (int i = 0; i < count; i++)
-  {
-    for (int j = 0; j < count - i - 1; j++)
-    {
-      if (p[j].id > p[j + 1].id)
-      {
+  for (int i = 0; i < count; i++) {
+    for (int j = 0; j < count - i - 1; j++) {
+      if (p[j].id > p[j + 1].id) {
         tmp = p[j];
         p[j] = p[j + 1];
         p[j + 1] = tmp;
       }
     }
   }
-  for (int i = 0; i < count; i++)
-  {
+  for (int i = 0; i < count; i++) {
     printf("|ID:       %d \n", p[i].id);
     printf("|Nom:      %s   \n", p[i].nom);
     printf("|Espece:   %s   \n", p[i].espece);
     printf("|Habitat:  %s   \n", p[i].habitat);
     printf("|Age:      %d   \n", p[i].age);
     printf("|Poids:    %.2fKg \n", p[i].poids);
-    if (p[i].age > 20)
-    {
+    if (p[i].age > 20) {
       printf("|---------------------------|\n");
       printf("!!!! AGE CRITIQUE !!!!\n");
     }
@@ -100,31 +87,25 @@ void affid(struct Animal p[])
   }
 }
 
-void affage(struct Animal p[])
-{
+void affage(struct Animal p[]) {
   struct Animal tmp;
-  for (int i = 0; i < count; i++)
-  {
-    for (int j = 0; j < count - i - 1; j++)
-    {
-      if (p[j].age > p[j + 1].age)
-      {
+  for (int i = 0; i < count; i++) {
+    for (int j = 0; j < count - i - 1; j++) {
+      if (p[j].age > p[j + 1].age) {
         tmp = p[j];
         p[j] = p[j + 1];
         p[j + 1] = tmp;
       }
     }
   }
-  for (int i = 0; i < count; i++)
-  {
+  for (int i = 0; i < count; i++) {
     printf("|ID:       %d \n", p[i].id);
     printf("|Nom:      %s   \n", p[i].nom);
     printf("|Espece:   %s   \n", p[i].espece);
     printf("|Habitat:  %s   \n", p[i].habitat);
     printf("|Age:      %d   \n", p[i].age);
     printf("|Poids:    %.2fKg \n", p[i].poids);
-    if (p[i].age > 20)
-    {
+    if (p[i].age > 20) {
       printf("|---------------------------|\n");
       printf("!!!! AGE CRITIQUE !!!!\n");
     }
@@ -132,31 +113,25 @@ void affage(struct Animal p[])
   }
 }
 
-void affn(struct Animal p[])
-{
+void affn(struct Animal p[]) {
   struct Animal tmp;
-  for (int i = 0; i < count; i++)
-  {
-    for (int j = 0; j < count - i - 1; j++)
-    {
-      if (strcmp(p[j].nom, p[j + 1].nom) > 0)
-      {
+  for (int i = 0; i < count; i++) {
+    for (int j = 0; j < count - i - 1; j++) {
+      if (strcmp(p[j].nom, p[j + 1].nom) > 0) {
         tmp = p[j];
         p[j] = p[j + 1];
         p[j + 1] = tmp;
       }
     }
   }
-  for (int i = 0; i < count; i++)
-  {
+  for (int i = 0; i < count; i++) {
     printf("|ID:       %d \n", p[i].id);
     printf("|Nom:      %s   \n", p[i].nom);
     printf("|Espece:   %s   \n", p[i].espece);
     printf("|Habitat:  %s   \n", p[i].habitat);
     printf("|Age:      %d   \n", p[i].age);
     printf("|Poids:    %.2fKg \n", p[i].poids);
-    if (p[i].age > 20)
-    {
+    if (p[i].age > 20) {
       printf("|---------------------------|\n");
       printf("!!!! AGE CRITIQUE !!!!\n");
     }
@@ -164,25 +139,21 @@ void affn(struct Animal p[])
   }
 }
 
-void affhabitat(struct Animal p[])
-{
+void affhabitat(struct Animal p[]) {
   char hab[10];
   printf("+--------------------------------------------+\n");
   printf("|Entrer Habitat:                             |\n");
   scanf("%s", hab);
   hab[0] = toupper(hab[0]);
-  for (int i = 0; i < count; i++)
-  {
-    if (strcmp(hab, p[i].habitat) == 0)
-    {
+  for (int i = 0; i < count; i++) {
+    if (strcmp(hab, p[i].habitat) == 0) {
       printf("|ID:       %d \n", p[i].id);
       printf("|Nom:      %s   \n", p[i].nom);
       printf("|Espece:   %s   \n", p[i].espece);
       printf("|Habitat:  %s   \n", p[i].habitat);
       printf("|Age:      %d   \n", p[i].age);
       printf("|Poids:    %.2fKg \n", p[i].poids);
-      if (p[i].age > 20)
-      {
+      if (p[i].age > 20) {
         printf("|---------------------------|\n");
         printf("!!!! AGE CRITIQUE !!!!\n");
       }
@@ -191,8 +162,7 @@ void affhabitat(struct Animal p[])
   }
 }
 
-void afficher(struct Animal p[])
-{
+void afficher(struct Animal p[]) {
   printf("+--------------------------------------------+\n");
   printf("|                    Afficher                |\n");
   printf("|--------------------------------------------|\n");
@@ -204,8 +174,7 @@ void afficher(struct Animal p[])
   printf("|Autre touche pour retourner.                |\n");
   printf("|--------------------------------------------|\n\n\n");
   scanf(" %c", &option);
-  switch (option)
-  {
+  switch (option) {
   case '1':
     affid(p);
     break;
@@ -224,17 +193,14 @@ void afficher(struct Animal p[])
   }
 }
 
-void modage(struct Animal p[])
-{
+void modage(struct Animal p[]) {
   int f;
   int exist = 0;
   printf("|     Entrer l'id l'animaux pour modifier l'age   |\n");
   printf("+-------------------------------------------------+\n");
   scanf("%d", &f);
-  for (int i = 0; i < count; i++)
-  {
-    if (f == p[i].id)
-    {
+  for (int i = 0; i < count; i++) {
+    if (f == p[i].id) {
       printf("- Entrer nouveau age :\n");
       scanf("%d", &p[i].age);
       exist = 1;
@@ -244,17 +210,14 @@ void modage(struct Animal p[])
     printf("C'et animaux n'exist pas !!\n");
 }
 
-void modhabit(struct Animal p[])
-{
+void modhabit(struct Animal p[]) {
   int f;
   int exist = 0;
   printf("|  Entrer l'id l'animaux pour modifier l'habitat  |\n");
   printf("+-------------------------------------------------+\n");
   scanf("%d", &f);
-  for (int i = 0; i < count; i++)
-  {
-    if (f == p[i].id)
-    {
+  for (int i = 0; i < count; i++) {
+    if (f == p[i].id) {
       printf("- Entrer Nouveau habitat d'animaux:\n");
       scanf("%s", p[i].habitat);
       p[i].habitat[0] = toupper(p[i].habitat[0]);
@@ -265,8 +228,7 @@ void modhabit(struct Animal p[])
     printf("C'et animaux n'exist pas !!\n");
 }
 
-void modifier(struct Animal p[])
-{
+void modifier(struct Animal p[]) {
   printf("+--------------------------------------------+\n");
   printf("|                    Modifier                |\n");
   printf("|--------------------------------------------|\n");
@@ -276,8 +238,7 @@ void modifier(struct Animal p[])
   printf("|Autre touche pour retourner.                |\n");
   printf("|--------------------------------------------|\n\n\n");
   scanf(" %c", &option);
-  switch (option)
-  {
+  switch (option) {
   case '1':
     modage(p);
     break;
@@ -290,19 +251,15 @@ void modifier(struct Animal p[])
   }
 }
 
-void supp(struct Animal p[])
-{
+void supp(struct Animal p[]) {
   int f;
   int exist = 0;
   printf("|  Entrer l'id d'animaux    |\n");
   printf("+---------------------------+\n");
   scanf("%d", &f);
-  for (int j = 0; j < count; j++)
-  {
-    if (f == p[j].id)
-    {
-      for (int i = j; i < count - 1; i++)
-      {
+  for (int j = 0; j < count; j++) {
+    if (f == p[j].id) {
+      for (int i = j; i < count - 1; i++) {
         p[i] = p[i + 1];
       }
       exist = 1;
@@ -314,8 +271,227 @@ void supp(struct Animal p[])
     printf("C'et animaux n'exist pas !!\n");
 }
 
-int main()
-{
+//
+// Search espece
+void sespece(struct Animal p[]) {
+  char f[10];
+  int exist = 0;
+
+  printf("| Entrer l'espece d'animaux |\n");
+  printf("+---------------------------+\n");
+  scanf("%s", f);
+  f[0] = toupper(f[0]);
+  for (int i = 0; i < count; i++) {
+    if (strcmp(f, p[i].espece) == 0) {
+      printf("|ID:       %d \n", p[i].id);
+      printf("|Nom:      %s   \n", p[i].nom);
+      printf("|Espece:   %s   \n", p[i].espece);
+      printf("|Habitat:  %s   \n", p[i].habitat);
+      printf("|Age:      %d   \n", p[i].age);
+      printf("|Poids:    %.2fKg \n", p[i].poids);
+      printf("|---------------------------|\n");
+      exist = 1;
+    }
+  }
+  if (exist == 0) {
+    printf("C'et animaux n'exist pas !!\n");
+  }
+}
+
+// Search Nom
+void snom(struct Animal p[]) {
+  char f[10];
+  int exist = 0;
+
+  printf("|  Entrer le nom  d'animaux |\n");
+  printf("+---------------------------+\n");
+  scanf("%s", f);
+  f[0] = toupper(f[0]);
+  for (int i = 0; i < count; i++) {
+    if (strcmp(f, p[i].nom) == 0) {
+      printf("|ID:       %d \n", p[i].id);
+      printf("|Nom:      %s   \n", p[i].nom);
+      printf("|Espece:   %s   \n", p[i].espece);
+      printf("|Habitat:  %s   \n", p[i].habitat);
+      printf("|Age:      %d   \n", p[i].age);
+      printf("|Poids:    %.2fKg \n", p[i].poids);
+      printf("|---------------------------|\n");
+      exist = 1;
+    }
+  }
+  if (exist == 0) {
+    printf("C'et animaux n'exist pas !!\n");
+  }
+}
+// Search id
+void sid(struct Animal p[]) {
+  int f;
+  int exist = 0;
+
+  printf("|  Entrer l'id d'animaux    |\n");
+  printf("+---------------------------+\n");
+  scanf("%d", &f);
+  for (int i = 0; i < count; i++) {
+    if (f == p[i].id) {
+
+      printf("|ID:       %d \n", p[i].id);
+      printf("|Nom:      %s   \n", p[i].nom);
+      printf("|Espece:   %s   \n", p[i].espece);
+      printf("|Habitat:  %s   \n", p[i].habitat);
+      printf("|Age:      %d   \n", p[i].age);
+      printf("|Poids:    %.2fKg \n", p[i].poids);
+      printf("|---------------------------|\n");
+      exist = 1;
+    }
+  }
+
+  if (exist == 0) {
+    printf("C'et animaux n'exist pas !!\n");
+  }
+}
+
+// Menu Search
+void chercher(struct Animal p[]) {
+  printf("+--------------------------------------------+\n");
+  printf("|                   Rechercher               |\n");
+  printf("|--------------------------------------------|\n");
+  printf("|1-Par Id.                                   |\n");
+  printf("|2-Par nom.                                  |\n");
+  printf("|3-Par espece.                               |\n");
+  printf("|--------------------------------------------|\n");
+  printf("|Autre touche pour retourner.                |\n");
+  printf("|--------------------------------------------|\n\n\n");
+
+  scanf(" %c", &option);
+  switch (option) {
+  case '1':
+    sid(p);
+    break;
+  case '2':
+    snom(p);
+    break;
+  case '3':
+    sespece(p);
+    break;
+  default:
+    system("clear");
+    break;
+  }
+}
+//
+
+// moyen age
+float moyage(struct Animal p[]) {
+  float moyen = 0;
+  for (int i = 0; i < count; i++) {
+    moyen = p[i].age + moyen;
+  }
+  moyen = moyen / count;
+  return moyen;
+}
+
+// min max
+void jeune(struct Animal p[]) {
+
+  int mindex = 0, maxdex = 0;
+  int min = p[0].age, max = p[0].age;
+
+  for (int i = 1; i < count; i++) {
+    if (p[i].age < min) {
+      min = p[i].age;
+      mindex = i;
+    }
+    if (p[i].age > max) {
+      max = p[i].age;
+      maxdex = i;
+    }
+  }
+
+  system("clear");
+  printf("|--------------------------------------------|\n");
+  printf("|Le plus jeune d'animaux:                    \n");
+  printf("|--------------------------------------------|\n\n\n");
+  printf("|ID:       %d \n", p[mindex].id);
+  printf("|Nom:      %s   \n", p[mindex].nom);
+  printf("|Espece:   %s   \n", p[mindex].espece);
+  printf("|Habitat:  %s   \n", p[mindex].habitat);
+  printf("|Age:      %d   \n", p[mindex].age);
+  printf("|Poids:   %.2fKg \n", p[mindex].poids);
+  printf("|--------------------------------------------|\n");
+  printf("|Le plus vieux d'animaux:                    \n");
+  printf("|--------------------------------------------|\n\n\n");
+  printf("|ID:       %d \n", p[maxdex].id);
+  printf("|Nom:      %s   \n", p[maxdex].nom);
+  printf("|Espece:   %s   \n", p[maxdex].espece);
+  printf("|Habitat:  %s   \n", p[maxdex].habitat);
+  printf("|Age:      %d   \n", p[maxdex].age);
+  printf("|Poids:   %.2fKg \n", p[maxdex].poids);
+}
+// plus frequent espece
+void frequent(struct Animal p[]) {
+  int representations[count];
+  int max = 0;
+  for (int i = 0; i < count; i++) {
+    representations[i] = 0;
+    for (int j = 0; j < count; j++) {
+      if (strcmp(p[j].espece, p[i].espece) == 0) {
+        representations[i]++;
+      }
+    }
+  }
+
+  char fespece[30];
+  for (int i = 0; i < count; i++) {
+    if (representations[i] > max) {
+      max = representations[i];
+      strcpy(fespece, p[i].espece);
+    }
+  }
+
+  printf("|--------------------------------------------|\n");
+  printf("Le plus represente est: %s %d. \n", fespece, max);
+  printf("|--------------------------------------------|\n");
+}
+
+// Stats menu
+void stat(struct Animal p[]) {
+  printf("+--------------------------------------------+\n");
+  printf("|                  Statistiques              |\n");
+  printf("|--------------------------------------------|\n");
+  printf("|1-Nombre total d'animaux.                   |\n");
+  printf("|2-Age Moyen des animaux.                    |\n");
+  printf("|3-Plus vieux et plus jeune animaux.         |\n");
+  printf("|4-L'animaux le plus represente.             |\n");
+  printf("|--------------------------------------------|\n");
+  printf("|Autre touche pour retourner.                |\n");
+  printf("|--------------------------------------------|\n\n\n");
+
+  scanf(" %c", &option);
+  switch (option) {
+  case '1':
+    printf("|--------------------------------------------|\n");
+    printf("|Nombre total des animaux %d                 |\n", count);
+    printf("|--------------------------------------------|\n\n\n");
+    break;
+  case '2':
+    printf("|--------------------------------------------|\n");
+    printf("|L'age Moyen des animaux: %.2f |\n", moyage(p));
+    printf("|--------------------------------------------|\n\n\n");
+    break;
+  case '3':
+    jeune(p);
+    break;
+  case '4':
+    frequent(p);
+    break;
+  default:
+    system("clear");
+    break;
+  }
+}
+
+//
+int main() {
   struct Animal animaux[200] = {
       {1, "Simba", "Lion", 5, "Savane", 190.5},
       {2, "Nala", "Lion", 4, "Savane", 175},
@@ -338,8 +514,7 @@ int main()
       {19, "Polly", "Oiseau", 5, "Jungle", 1.5},
       {20, "Kong", "Gorille", 13, "Jungle", 180}};
 
-  while (option != '8')
-  {
+  while (option != '8') {
     printf("+============================================+\n");
     printf("|                    ZOO                     |\n");
     printf("+============================================+\n");
@@ -354,8 +529,7 @@ int main()
     printf("+============================================+\n");
     scanf(" %c", &option);
     system("clear");
-    switch (option)
-    {
+    switch (option) {
     case '1':
       animaux[count++] = ajouter();
       break;
@@ -369,11 +543,13 @@ int main()
       modifier(animaux);
       break;
     case '5':
+      chercher(animaux);
       break;
     case '6':
       supp(animaux);
       break;
     case '7':
+      stat(animaux);
       break;
     case '8':
       printf("Au Revoir.\n");
